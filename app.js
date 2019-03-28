a6517d655c4b3a838ac47f9b492a2f64a047761e
Kusjay <kelechiuma@gmail.com>
2019-03-28 15:18:02 +0100

    local authentication with passport

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const passport = require('passport');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Connect to mongoose
 mongoose.connect('mongodb://localhost/ideabox-dev', {
@@ -15,9 +24,6 @@ mongoose.connect('mongodb://localhost/ideabox-dev', {
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-// Load Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -29,6 +35,9 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Method Override middleware
 app.use(methodOverride('_method'));
 
@@ -39,6 +48,10 @@ app.use(session({
   saveUninitialized: true,
 }));
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect flash middleware
 app.use(flash());
 
@@ -47,6 +60,7 @@ app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
@@ -61,95 +75,11 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-// Idea Index Page
-app.get('/ideas', (req, res) => {
-  Idea.find({})
-  .sort({date:'desc'})
-  .then(ideas => {
-    res.render('ideas/index', {
-      ideas: ideas
-    });
-  });
-});
+/// Use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-  res.render('ideas/add');
-});
-
-// Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-  Idea.findOne({
-    _id: req.params.id
-  })
-  .then(idea => {
-    res.render('ideas/edit', {
-      idea: idea
-    });
-  });
-});
-
-// Process Add Form
-app.post('/ideas', (req, res) => {
-  let errors = [];
-
-  if(!req.body.title){
-    errors.push({text: 'Please add a title'});
-  }
-  if(!req.body.details){
-    errors.push({text: 'Please add some details'});
-  }
-
-  if(errors.length > 0){
-    res.render('ideas/add', {
-      errors: errors,
-      title: req.body.title,
-      details: req.body.details
-    });
-  } else {
-    const newUser = {
-      title: req.body.title,
-      details: req.body.details
-    }
-    new Idea(newUser)
-      .save()
-      .then(idea => {
-        req.flash('success_msg', 'Idea added');
-        res.redirect('/ideas');
-      })
-  }
-});
-
-// Process Edit Form
-app.put('/ideas/:id', (req, res) => {
-  Idea.findOne({
-    _id: req.params.id
-  })
-  .then(idea => {
-    // update the values
-    idea.title = req.body.title;
-    idea.details = req.body.details;
-
-    idea.save()
-      .then(idea => {
-        req.flash('success_msg', 'Idea updated');
-        res.redirect('/ideas');
-      })
-  })
-});
-
-// Delete Idea
-app.delete('/ideas/:id', (req, res) => {
-  Idea.deleteOne({
-    _id: req.params.id
-  })
-    .then(() => {
-      req.flash('success_msg', 'Idea removed');
-      res.redirect('/ideas');
-    });
-});
-
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
